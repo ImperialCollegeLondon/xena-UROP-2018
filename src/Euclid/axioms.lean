5de6cbf4f949a1e8d2add8d7343927bba15fef16
Ali Sever <alisever96@hotmail.com>
2018-07-11 15:52:18 +0100

    added some axioms

--- src/Euclid/axioms.lean
+++ src/Euclid/axioms.lean
@@ -1,21 +1,21 @@
-import Euclid.unordered_pairs
-
-#check uprod 
-
 structure Euclidean_plane :=
+
 (point : Type)
--- distance between two points
-(equidist : point → point → point → point → Prop)
+-- Equidistance of 4 Points
+(eqd : point → point → point → point → Prop)
 -- between A B C means B is on the line segment AC
 (B : point → point → point → Prop)
-(reflex : ∀ a b : point, equidist a b b a)
-(trans : ∀ a b p q r s, equidist a b p q → equidist p q r s → equidist a b r s)
-(id_equidist : ∀ a b c, equidist a b c c → a = b)
+(reflex : ∀ a b : point, eqd a b b a)
+(trans : ∀ a b p q r s, eqd a b p q → eqd p q r s → eqd a b r s)
+(id_eqd : ∀ a b c, eqd a b c c → a = b)
+(segment_construction : ∀ a b c q, ∃ x, B q a x → eqd a x b c)
+(five_segment : ∀ a b c d a' b' c' d', a ≠ b → B a b c → B a' b' c' → eqd a b a' b' → eqd b c b' c' 
+→ eqd a d a' d' → eqd b d b' d' → eqd c d c' d')
+(betweenness : ∀ a b, B a b a → a = b)
+(inner_pasch : ∀ a b c p q, B a p c → B b q c → ∃ x, B p x b → B q x a)
+
+
 (continuity : ∀ X Y : set point, 
   (∃ a, ∀ x y, x ∈ X → y ∈ Y → B a x y) → (∃ b, ∀ x y, x ∈ X → y ∈ Y → B x b y)
 )
-
-
-open Euclidean_plane 
-variable {EP : Euclidean_plane}
 
