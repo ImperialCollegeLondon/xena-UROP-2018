a880b9315b4fc2c5063e822129c6534da4e689f3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-11 14:15:36 +0100

    adding schema

--- src/Euclid/axioms.lean
+++ src/Euclid/axioms.lean
@@ -7,11 +7,13 @@ structure Euclidean_plane :=
 -- distance between two points
 (equidist : point → point → point → point → Prop)
 -- between A B C means B is on the line segment AC
-(between : point → point → point → Prop)
+(B : point → point → point → Prop)
 (reflex : ∀ a b : point, equidist a b b a)
 (trans : ∀ a b p q r s, equidist a b p q → equidist p q r s → equidist a b r s)
 (id_equidist : ∀ a b c, equidist a b c c → a = b)
-
+(continuity : ∀ X Y : set point, 
+  (∃ a, ∀ x y, x ∈ X → y ∈ Y → B a x y) → (∃ b, ∀ x y, x ∈ X → y ∈ Y → B x b y)
+)
 
 
 open Euclidean_plane 
