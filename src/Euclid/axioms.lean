653e0f12c838cf9adcadffa9d4b86a17ba1bc865
Ali Sever <alisever96@hotmail.com>
2018-07-11 21:14:54 +0100

    Axioms

--- src/Euclid/axioms.lean
+++ src/Euclid/axioms.lean
@@ -4,13 +4,13 @@ class Euclidean_plane (point : Type) :=
 -- between A B C means B is on the line segment AC
 (B : point → point → point → Prop)
 
-(reflex : ∀ a b : point, eqd a b b a)
-(trans : ∀ a b p q r s, eqd a b p q → eqd a b r s → eqd p q r s)
+(euc_refl : ∀ a b : point, eqd a b b a)
+(euc_trans : ∀ a b p q r s, eqd a b p q → eqd a b r s → eqd p q r s)
 (id_eqd : ∀ a b c, eqd a b c c → a = b)
 (segment_construction : ∀ a b c q, ∃ x, B q a x → eqd a x b c)
 (five_segment : ∀ a b c d a' b' c' d', a ≠ b → B a b c → B a' b' c' → eqd a b a' b' 
 → eqd b c b' c' → eqd a d a' d' → eqd b d b' d' → eqd c d c' d')
-(betweenness : ∀ a b, B a b a → a = b)
+(between_one_point : ∀ a b, B a b a → a = b)
 (inner_pasch : ∀ a b c p q, B a p c → B b q c → ∃ x, B p x b → B q x a)
 (two_dimensions : ∃ a b c, ¬B a b c → ¬B b c a → ¬B c a b)
 (not_three_dimensions : ∀ a b c p₁ p₂, p₁ ≠ p₂ → eqd a p₁ a p₂ → eqd b p₁ b p₂ 
@@ -39,5 +39,8 @@ open Euclidean_plane
 
 variables {point : Type} [Euclidean_plane point]
 
+example (a b : point) : eqd a b a b :=
+sorry
+
 example (a b : point) : eqd a a b b :=
-    sorry
+sorry
