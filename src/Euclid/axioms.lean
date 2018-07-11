8146648c3d166b86965271fe906e5bc167383f0b
Ali Sever <alisever96@hotmail.com>
2018-07-11 16:47:08 +0100

    added axioms

--- src/Euclid/axioms.lean
+++ src/Euclid/axioms.lean
@@ -9,13 +9,28 @@ structure Euclidean_plane :=
 (trans : ∀ a b p q r s, eqd a b p q → eqd p q r s → eqd a b r s)
 (id_eqd : ∀ a b c, eqd a b c c → a = b)
 (segment_construction : ∀ a b c q, ∃ x, B q a x → eqd a x b c)
-(five_segment : ∀ a b c d a' b' c' d', a ≠ b → B a b c → B a' b' c' → eqd a b a' b' → eqd b c b' c' 
-→ eqd a d a' d' → eqd b d b' d' → eqd c d c' d')
+(five_segment : ∀ a b c d a' b' c' d', a ≠ b → B a b c → B a' b' c' → eqd a b a' b' 
+→ eqd b c b' c' → eqd a d a' d' → eqd b d b' d' → eqd c d c' d')
 (betweenness : ∀ a b, B a b a → a = b)
 (inner_pasch : ∀ a b c p q, B a p c → B b q c → ∃ x, B p x b → B q x a)
---something
+(two_dimensions : ∃ a b c, ¬B a b c → ¬B b c a → ¬B c a b)
+(not_three_dimensions : ∀ a b c p₁ p₂, p₁ ≠ p₂ → eqd a p₁ a p₂ → eqd b p₁ b p₂ 
+→ eqd c p₁ c p₂ → (B a b c ∨ B b c a ∨ B c a b))
+(euclids : ∀ a b c d t, B a d t → B b d c → a ≠ d → ∃ x y, B a b x → B a c y → B y t x)
+(reflexivity_betweenness : ∀ a b, B a b b)
+(between_itself : ∀ a b, a = b → B a b a)
+(inner_trans : ∀ a b c d, B a b d → B b c d → B a b c)
+(outer_trans : ∀ a b c d, B a b c → B b c d → b ≠ c → B a b d)
+(inner_connectivity : ∀ a b c d, B a b d → B a c d → (B a b c ∨ B a c b))
+(outer_connectivity : ∀ a b c d, B a b c → B a b d → a ≠ b → (B a c d ∨ B a d c))
+(equal_distances : ∀ a b c, a = b → eqd a c b c)
+(unique_triangle : ∀ a b c d c' d' x, eqd a c a c' → eqd b c b c' → B a d b → B a d' b 
+→ B c d x → B c' d' x → d ≠ x → d' ≠ x → c = c')
+(existence_triangle : ∀ a b a' b' c' p, eqd a b a' b' → ∃ c x, eqd a c a' c' → eqd b c b' c' 
+→ B c x p → (B a b x ∨ B b x a ∨ B x a b))
+(density_betweenness : ∀ x y z, x ≠ z → ∃ y, x ≠ y → z ≠ y → B x y z)
+
 
 (continuity : ∀ X Y : set point, 
-  (∃ a, ∀ x y, x ∈ X → y ∈ Y → B a x y) → (∃ b, ∀ x y, x ∈ X → y ∈ Y → B x b y)
-)
+  (∃ a, ∀ x y, x ∈ X → y ∈ Y → B a x y) → (∃ b, ∀ x y, x ∈ X → y ∈ Y → B x b y))
 
