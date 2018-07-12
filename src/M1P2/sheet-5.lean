003a69aee71c17d3f6f8be13b8cc8316b41573bb
Elliott Macneil <32262318+emacneil98@users.noreply.github.com>
2018-07-12 17:45:43 +0100

    Update sheet-5.lean
    Formalised the questions for sheet 5 groups.

--- src/M1P2/sheet-5.lean
+++ src/M1P2/sheet-5.lean
@@ -1,18 +1,22 @@
--- sheet 5
+import group_theory.subgroup data.set.basic algebra.group data.equiv
 
-variables (H) (K) 
+-- sheet 5
+variables {G : Type*} [group G]
+variables (H K : set G)
 variables [is_subgroup H] [is_subgroup K]
 
 -- 3. Let G be a group with subgroups H and K.
 
 -- (a) Prove that H ∩ K is a subgroup of G.
-theorem sheet05_q3a:
+theorem sheet05_q3a: is_subgroup(H ∩ K) := sorry
 -- (b) Show that H ∪ K is not a subgroup, unless either H ⊆ K or K ⊆ H.
-theorem sheet05_q3b:
+theorem sheet05_q3b (hp:¬(H ⊆ K ∨ K ⊆ H)): ¬is_subgroup(H ∪ K) := sorry
 
 -- 4. Prove the following statements.
-theorem sheet05_q3c:
+definition is_cyclic (G : Type*) [group G] := ∃ x : G, gpowers x = set.univ
 -- (a) Every cyclic group is abelian.
-theorem sheet05_q4a:
--- (b) The group Sn is not abelian, unless n < 3.
-theorem sheet05_q4b:
+theorem sheet05_q4a : is_cyclic G → comm_group G := sorry
+--{ ..is_subgrou }
+definition gsymmetric (n : ℕ) := equiv.perm (fin n)
+-- (b) The symmetric group is not abelian, unless n < 3.
+--theorem sheet05_q4b (n : ℕ) (n ≥ 4) : gsymmetric n → ¬(comm_group (gsymmetric n)) := sorry 
