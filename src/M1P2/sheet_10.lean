cdd1ff09faf986ce5fa9c460201e8328dc8c13fa
elliott98 <elliott.macneil17@imperial.ac.uk>
2018-07-25 12:28:05 +0100

    minor updates to M1P2 sheets

--- src/M1P2/sheet_10.lean
+++ src/M1P2/sheet_10.lean
@@ -1,17 +1,22 @@
-import algebra.ring algebra.group_power data.nat.prime data.nat.basic data.int.modeq algebra.group_power group_theory.subgroup algebra.group data.set.basic data.nat.gcd init.algebra.ring data.complex.basic data.finsupp data.nat.sqrt algebra.euclidean_domain
+import data.nat.prime data.int.modeq data.complex.basic algebra.euclidean_domain number_theory.pell data.nat.sqrt
 open nat 
+
 --1. For each of the following rings R, and elements a and b, find d = hcf(a,b), and calculate s, t ∈ R such that as + bt = d.
-variables {R : Type*} [ring R]
+variables {R : Type*} [euclidean_domain R]
 variables a b : ℤ
 
 --def d : ℤ := gcd a b
 
-def gaussian_ints : ℂ := ⟨a,b⟩ 
+structure gaussian_ints := 
+(re : ℤ)
+(im : ℤ)
 
-def poly_coeff_rat := list rat -- could also use finsupp
+def gaussian_val (x : gaussian_ints) : ℕ := (x.re * x.re + x.im * x.im).nat_abs
 
-def ints_adj_sqrt (n : ℤ) := 
-set.range (λ x : ℤ × ℤ, x.1 + x.2 * nat.sqrt n.nat_abs)
+--def poly_coeff_rat := list rat -- could also use finsupp
+
+--def ints_adj_sqrt (n : ℤ) := 
+--set.range (λ x : ℤ × ℤ, x.1 + x.2 * nat.sqrt n.nat_abs)
 
 --(a) a=4+3i,b=2−4i ∈ Z[i].
 --theorem sheet10_q1_a (x y : gaussian_ints) (ha : a = ⟨4,3⟩) (ha : b = ⟨2,-4⟩)
@@ -23,10 +28,11 @@ set.range (λ x : ℤ × ℤ, x.1 + x.2 * nat.sqrt n.nat_abs)
 --theorem sheet10_q1_c (x y : poly_coeff_rat) (ha x = [-1,-1,-1,0,0,1]) (hb y = [1,1,2,0,1]) : 
 
 --2. Find an irreducible element of ℤ[√3] that is not prime
+  -- def irreducible (α : R) :
     -- e.g : 2 + √3 i think works
 
 --3. Show that R = ℤ[√3] is a Euclidean domain with Euclidean function If a=1+3 3,b=3−4 3inZ[√3],find d=hcf(a,b), and calculate δ(a) = |N(a)|, where N is the norm map.s, t ∈ R such that as + bt = d.
-def R' := ints_adj_sqrt 3
+--def R' := ints_adj_sqrt 3
 -- def euc_func () := 
 --theorem sheet10_q3 
 
