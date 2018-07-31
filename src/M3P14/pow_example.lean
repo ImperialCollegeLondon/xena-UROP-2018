105127bd40e1abf330f9ec3939cc27623f5e2e80
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-07-31 23:13:37 +0100

    example of argument which switches from nat to int to Z/p

--- src/M3P14/pow_example.lean
+++ src/M3P14/pow_example.lean
@@ -1,17 +1,32 @@
--- UNFINISHED -- WORK IN PROGRESS. KMB
-
 import xenalib.nat_stuff
 import chris_hughes_various.zmod
 
 /- Here is my memory of the situation Clara and Jason were faced with -/
-set_option pp.all true
-example (a : ℕ) (p : ℕ) (x : ℤ) 
+--set_option pp.all true
+--set_option pp.notation false
+example (a : ℕ) (p : ℕ) [pos_nat p] (Hodd : 2 ∣ (p - 1)) (x : ℤ) 
   (H1 : ↑(a ^ (p - 1)) - 1 ≡ 0 [ZMOD ↑p]) 
   (H2 : x ≡ (↑a)^2 [ZMOD ↑p]) :
 x ^ ((p-1) / 2) ≡ 1 [ZMOD ↑p] :=
 -- A mathematician would just say "substitute x = a^2 and we're done"
 -- Lean says "naturals aren't integers, and congruence is not equality, so no"
--- I say "that's OK, we just explain to Lean that things which are "obviously equal" are equal"
+-- I say "that's OK, we just explain to Lean exactly why things which are
+-- "obviously equal" are equal"
 begin
-  rw @nat.cast_pow ℤ _ a 2 at H2,
+  rw nat.cast_pow' a 2 at H2,
+  rw ←zmod.eq_iff_modeq_int at H2,
+  -- H2 now an equality in the ring Z/pZ
+  rw ←zmod.eq_iff_modeq_int,
+  rw ←int.cast_pow,
+  -- we can finally rewrite!
+  rw H2,
+  -- we now have to convince Lean that this is just H1
+  rw int.cast_pow,
+  rw nat.cast_pow',
+  rw nat.pow_pow,
+  rw nat.mul_div (show 2 ≠ 0, from dec_trivial) Hodd,
+  apply eq_of_sub_eq_zero,
+  rw ←int.cast_sub,
+  rw ←zmod.eq_iff_modeq_int at H1,
+  exact H1
 end 
--- src/xenalib/nat_stuff.lean
+++ src/xenalib/nat_stuff.lean
@@ -1,7 +1,36 @@
 import algebra.group_power
+import data.nat.basic data.nat.cast
+
+#check nat.mul
+
 
 theorem nat.cast_pow {α : Type*} [semiring α] (n : ℕ) : ∀ m : ℕ, (n : α) ^ m = (n ^ m : ℕ)
 | 0 := nat.cast_one.symm 
 | (d+1) := show ↑n * ↑n ^ d = ↑(n ^ d * n), by rw [nat.cast_pow d,mul_comm,nat.cast_mul]
 
- theorem nat.pow_two (a : ℕ) : a ^ 2 = a * a := show (1 * a) * a = _, by rw one_mul 
+theorem int.cast_pow {α : Type*} [ring α] (n : ℤ) : ∀ m : ℕ, (n : α) ^ m = (n ^ m : ℤ)
+| 0 := nat.cast_one.symm 
+| (d+1) := show ↑n * ↑n ^ d = ↑(n * n ^ d), by rw [int.cast_pow d,int.cast_mul]
+
+theorem nat.cast_pow' (n : ℕ) : ∀ m : ℕ, (n : ℤ) ^ m = (n ^ m : ℕ)
+| 0 := nat.cast_one.symm 
+| (d+1) := show ↑n * ↑n ^ d = ↑(n ^ d * n),
+-- by rw [nat.cast_pow' d,mul_comm,nat.cast_mul]
+begin
+rw nat.cast_pow' d,
+rw mul_comm,
+simp,
+end 
+
+theorem nat.pow_pow (n a : ℕ) : ∀ b : ℕ, (n ^ a) ^ b = n ^ (a * b)
+| 0 := by rw mul_zero;refl
+| (d+1) := show (n^a)^d * n^a = _, by rw [mul_add,mul_one,nat.pow_add,nat.pow_pow]
+
+theorem nat.mul_div {a b : ℕ} : a ≠ 0 → a ∣ b → a * (b / a) = b :=
+begin
+  intros Ha Hd,
+  rw nat.dvd_iff_mod_eq_zero at Hd,
+  exact zero_add (a * (b / a)) ▸ (Hd ▸ nat.mod_add_div b a)
+end 
+
+theorem nat.pow_two (a : ℕ) : a ^ 2 = a * a := show (1 * a) * a = _, by rw one_mul 
