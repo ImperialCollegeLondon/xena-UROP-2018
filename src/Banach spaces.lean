430c985bc5f3f57aa4e85e466648943d214e3971
Minh Hieu Le (Kai) <hieule3004@gmail.com>
2018-08-01 15:09:09 +0100

    proved banach is metric

--- src/Banach spaces.lean
+++ src/Banach spaces.lean
@@ -8,48 +8,34 @@ section norm_space
 class banach_space (V : Type) extends norm_space V: Type :=
 (complete: complete_space ℝ)
 
-variables (v : Type) [norm_space V]
-def norm : V → ℝ := norm_space.N
-
-#check @eq.trans 
--- Incorrect, working on it atm
 noncomputable lemma banach_is_metric [h : banach_space V] : metric_space V :=
 {
 dist := by {
     intros x y,
     have n := h.N,
     exact n (x - y) },
-dist_self := by { 
+dist_self := by {
+    have d := h.norm_pos_def,
     intro,
-    have d := h.norm_pos_def,
     rw [sub_self],
-    apply (d (0 : V)).mpr,
-    exact eq.refl (0 : V) },
+    apply (d 0).mpr, 
+    refl },
 eq_of_dist_eq_zero := by {
-    intros x y hxy,
     have d := h.norm_pos_def,
+    intros x y hxy,
     replace d := (d (x - y)).mp hxy,
     replace d := congr_arg (+ y) d,
-    simp at d,
-    have a := add_zero y,
-    exact eq.trans d a },
-dist_comm := 
-    begin
-    intros,
-    dunfold herm_dist, 
-    dunfold herm_norm,
-    rw sqrt_inj (is_pos_def (x - y)).left (is_pos_def (y - x)).left,
-    simp, 
-    end,
-dist_triangle := 
-    begin 
-    dunfold herm_dist,
-    intros,
-    have H : x - z = (x - y) + (y - z),
-        simp,
-    rw H, 
-    exact norm_sub_add (x - y) (y - z),
-    end,
+    simp at d, exact d },
+dist_comm := by {
+    have d := h.norm_abs_hom,
+    intros x y,
+    replace d := d (y - x) (-1), 
+    simp at d, exact d },
+dist_triangle := by {
+    have d := h.norm_sub_add,
+    intros x y z,
+    replace d := d (x - y) (y - z),
+    simp at d, exact d },
 }
 
 end norm_space
