9c3a8ed50a864e369448097ce6de1043082b4713
guy <guy.leroy99@gmail.com>
2018-08-07 15:09:59 +0100

    changes to the equiv proof

--- src/M3P14/order_zmodn.lean
+++ src/M3P14/order_zmodn.lean
@@ -6,10 +6,14 @@ instance (n : nat) : pos_nat (nat.succ n) := ⟨nat.succ_pos _⟩
 instance : decidable_eq (units (zmod 5)) := 
 λ x y, decidable_of_iff _ ⟨ units.ext, λ _,by simp *⟩
 
+/-
+set_option pp.notation false
+set_option pp.implicit true
+-/
 theorem coprime_zmodn_units (n : ℕ) [pos_nat n] : equiv (units (zmod n)) {a : zmod n // ∃ b, a * b = 1} :=
 { to_fun := λ u, ⟨u.1 , u.2, u.3⟩,
 inv_fun := λ A, {val := (A.val).val, inv := ((A.val).val⁻¹),
-val_inv :=  begin
+val_inv :=  begin 
 cases (classical.em (n > 1)),
 {
     have eq : ∃ (b : zmod n), A.val * b = 1, from A.2,
@@ -18,26 +22,38 @@ cases (classical.em (n > 1)),
     rw zmod.mul_inv_eq_gcd n (A.val.val),
     have h3 : (A.val * a_1).val = (1 : zmod n).val , simp [a_2],
     rw zmod.mul_val at h3,
-    have h5 : (A.val).val * a_1.val % n = 1 % n, simp [h3],
-    {
-
-        sorry,
-    },
+    have h5 : (A.val).val * a_1.val % n = 1 % n, simp [h3], refl,
     have h6 : (A.val).val * a_1.val ≡ 1 [MOD n], by rwa nat.modeq,
     have h4 :  ↑n ∣ ↑((A.val).val * a_1.val) - ↑1, from nat.modeq.dvd_of_modeq h6.symm,
     rw nat.gcd_comm,
-    rw nat.gcd_rec,
-    
-    suffices : nat.coprime ((A.val).val % n) n, exact this,
-
-    --rw ←h3,
-    have h7 : ∃ (c : ℤ), ↑((A.val).val * a_1.val) - ↑1 = ↑n * c, from exists_eq_mul_right_of_dvd h4,
-    apply exists.elim h7,
+    have h7 : nat.gcd n ((A.val).val) ∣ ((A.val).val), from nat.gcd_dvd_right _ _, 
+    have h7' : nat.gcd n ((A.val).val) ∣ ((A.val).val) * a_1.val, from dvd_mul_of_dvd_left h7 a_1.val,    
+    have h8 : ∃ (c : ℤ), ↑((A.val).val * a_1.val) - ↑1 = ↑n * c, from exists_eq_mul_right_of_dvd h4,
+    apply exists.elim h8,
     intros,
-    --rw nat.modeq.modeq_iff_dvd at h3,
-    --have h4 : 
+    have h9 : (nat.gcd n ((A.val).val) : ℤ) ∣ n , from int.coe_nat_dvd.mpr (nat.gcd_dvd_left _ _),
+    have h10 : (nat.gcd n ((A.val).val) : ℤ) ∣ n * a, from dvd_mul_of_dvd_left h9 a, 
 
-    sorry,
+    have a_nat : a = (int.nat_abs a : ℤ), 
+    {
+        sorry,
+    },
+    have h11 : (nat.gcd n ((A.val).val) : ℤ) ∣ n * (int.nat_abs a : ℤ), rwa a_nat at h10,
+    have h12 : nat.gcd n ((A.val).val) ∣ n * int.nat_abs a, from int.coe_nat_dvd.mp h11,
+    have uneq : ((A.val).val * a_1.val) ≥ n * int.nat_abs a,
+    {
+        sorry
+    },
+
+    have dvd_sub : nat.gcd n ((A.val).val) ∣ (((A.val).val) * a_1.val) - (n * int.nat_abs a), from nat.dvd_sub uneq h7' h12 ,
+    have eq_1 : (((A.val).val) * a_1.val) - (n * int.nat_abs a) = 1,
+    {
+        sorry
+    },
+    have gcd_dvd_1 : nat.gcd n ((A.val).val) ∣ 1, by rwa eq_1 at dvd_sub,
+    have gcd_eq_1 : nat.gcd n ((A.val).val) = 1, from nat.dvd_one.mp gcd_dvd_1,
+    rw gcd_eq_1,
+    simp,
 },
 {
     sorry
@@ -53,9 +69,13 @@ sorry end,
 right_inv := begin sorry end,
 }
 
-#print nat.gcd
+#check nat.dvd_sub
 #check exists_eq_mul_right_of_dvd
 #check int.coe_nat_eq
+#check nat.coprime.coprime_dvd_left
+#check int.coe_nat_dvd
+#check eq.subst 
+#check nat.dvd_one
 
 --instance fintype_units_zmodn (n : ℕ) : fintype {a // 0 ≤ a ∧ a < n ∧ nat.gcd a n = 1} := sorry
 
