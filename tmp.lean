bd5751714b2dd07009d7160b424a6b9a7dcdeea2
mhl1817 <mhl1817@ic.ac.uk>
2018-08-10 04:57:36 +0100

    tmp

--- tmp.lean
+++ tmp.lean
@@ -18,12 +18,12 @@ include n
 
 -- infix ` • ` := has_scalar.smul
 
-def has_add : has_add (vector ℝ n) := 
-{ add := map₂ real.has_add.add }
+-- def has_add : has_add (vector ℝ n) := 
+-- { add := map₂ real.has_add.add }
 
-def has_add.add := @has_add.add _ (@has_add n)
+-- def has_add.add := @has_add.add _ (@has_add n)
 
-infix ` + ` := has_add.add
+-- infix ` + ` := has_add.add
 
 -- def has_zero : has_zero (vector ℝ n) :=
 -- { zero := repeat 0 n }
@@ -35,15 +35,47 @@ infix ` + ` := has_add.add
 set_option pp.all false
 -- set_option pp.all true
 
+-- def has_add.add : vector ℝ n → vector ℝ n → vector ℝ n := 
+-- map₂ real.has_add.add
+
+-- instance add_semigroup : add_semigroup (vector ℝ n) :=
+-- { 
+--     add := has_add.add,
+--     add_assoc := by
+--         { intros a b c, simp,
+--         cases a with a la,
+--         cases b with b lb,
+--         cases c with c lc,
+--         unfold has_add.add map₂, simp,
+--         induction n with n ih generalizing a b c,
+--             { have := list.length_eq_zero.mp la,
+--             have := list.length_eq_zero.mp lc,
+--             simp *,
+--             rw [list.nil_map₂, list.map₂_nil, list.nil_map₂] },
+--             { 
+--                 cases a with ha ta, contradiction,
+--                 cases b with hb tb, contradiction,
+--                 cases c with hc tc, contradiction,
+--                 unfold list.map₂,
+--                 apply congr, simp,
+--                 simp [nat.add_one] at la lb lc,
+--                 exact ih _ _ _ la lb lc } }
+-- }
+
+-- instance add_comm_semigroup : add_comm_semigroup (vector ℝ n) :=
+-- by { 
+--     have := add_semigroup,
+-- }
+
 instance : add_comm_group (vector ℝ n) :=
 {
-    add := map₂ real.has_add.add,
+    add := has_add.add,
     add_assoc := by 
         { intros a b c, simp,
         cases a with a la,
         cases b with b lb,
         cases c with c lc,
-        unfold map₂, simp,
+        unfold has_add.add map₂, simp,
         induction n with n ih generalizing a b c,
             { have := list.length_eq_zero.mp la,
             have := list.length_eq_zero.mp lc,
@@ -58,15 +90,16 @@ instance : add_comm_group (vector ℝ n) :=
                 simp [nat.add_one] at la lb lc,
                 exact ih _ _ _ la lb lc } },
     add_comm := by 
-        { intros a b,
-        
+        { intros a b, 
+        cases a with a la,
+        cases b with b lb,
          },
     neg := (map ∘ real.has_mul.mul) (-1),
     add_left_neg := by simp,
     zero := @repeat ℝ 0 n,
     zero_add := by 
         { intro a, simp,
-        apply vector.eq, unfold to_list,
+        apply vector.eq, unfold has_add.add to_list,
         cases a with a la, 
         unfold repeat map₂, simp,
         induction n with n ih generalizing a; 
