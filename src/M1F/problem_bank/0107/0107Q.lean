7440fcab41077f035ffbacd24a2f66ba5a87d821
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-08-13 17:42:04 +0100

    more 0107 tinkering

--- src/M1F/problem_bank/0107/0107Q.lean
+++ src/M1F/problem_bank/0107/0107Q.lean
@@ -3,16 +3,20 @@ import data.real.basic
 
 -- useful lemmas
 import xenalib.M1F.Q0107
+noncomputable theory
 
 def A : set ℝ := { x | x^2 < 3}
 def B : set ℝ := { x | x^2 < 3 ∧ ∃ y : ℤ, x = y}
 def C : set ℝ := { x | x^3 < 3}
 
+definition real_half := (1 / 2 : ℝ)
 -- useful lemmas
-#check (real_half_not_an_integer : ¬∃ (n : ℤ), 1 / 2 = ↑n)
+#check (real_half_not_an_integer : ¬∃ (n : ℤ), real_half = ↑n)
 #check (B_is_minus_one_zero_one : ∀ (x : ℝ), x ∈ B ↔ x = -1 ∨ x = 0 ∨ x = 1)
 
--- For each part x, prove exactly one of part_x_true and part_x_false!
+-- For each part x, prove exactly one of part_x_true and
+-- part_x_false, and comment out the other one!
+
 theorem part_a_true : (1/2 : ℝ) ∈ A ∩ B := sorry
 theorem part_a_false : ¬ (1/2 : ℝ) ∈ A ∩ B := sorry
 theorem part_b_true : (1/2 : ℝ) ∈ A ∪ B := sorry
