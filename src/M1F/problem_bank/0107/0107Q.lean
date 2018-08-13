24a048346cb8b613798b0dbc47e5011dffa803e0
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-08-13 17:25:44 +0100

    M1F sheet 1 Q 7 more work

--- src/M1F/problem_bank/0107/0107Q.lean
+++ src/M1F/problem_bank/0107/0107Q.lean
@@ -1,9 +1,18 @@
-import data.real.basic 
+-- real numbers
+import data.real.basic
+
+-- useful lemmas
+import xenalib.M1F.Q0107
 
 def A : set ℝ := { x | x^2 < 3}
 def B : set ℝ := { x | x^2 < 3 ∧ ∃ y : ℤ, x = y}
 def C : set ℝ := { x | x^3 < 3}
 
+-- useful lemmas
+#check (real_half_not_an_integer : ¬∃ (n : ℤ), 1 / 2 = ↑n)
+#check (B_is_minus_one_zero_one (x : ℝ) : x ∈ B ↔ x = -1 ∨ x = 0 ∨ x = 1)
+
+-- For each part x, prove exactly one of part_x_true and part_x_false!
 theorem part_a_true : (1/2 : ℝ) ∈ A ∩ B := sorry
 theorem part_a_false : ¬ (1/2 : ℝ) ∈ A ∩ B := sorry
 theorem part_b_true : (1/2 : ℝ) ∈ A ∪ B := sorry
