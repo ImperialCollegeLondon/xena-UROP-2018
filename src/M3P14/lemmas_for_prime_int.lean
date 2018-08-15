6c8f0df6bd38d0aca7915211306aff29ed22a705
sj2816 <sangwoo.jo16@imperial.ac.uk>
2018-08-15 16:31:12 +0100

    More about fermat_two_square theorem

--- src/M3P14/lemmas_for_prime_int.lean
+++ src/M3P14/lemmas_for_prime_int.lean
@@ -24,6 +24,18 @@ if quadratic_res a p ∧ ¬ ((p : ℤ) ∣ a) then 1 else
 if ¬ quadratic_res a p then -1 
 else 0
 
+theorem legendre_sym_one_implies_quadratic_res {p : ℤ} (a : ℤ)(H1 : prime_int p ∧ p ≠ 2): legendre_sym a H1 = 1 → quadratic_res a p :=
+begin 
+intro h,
+unfold legendre_sym at h,
+split_ifs at h, 
+exact h_1.1,
+exfalso,
+revert h, norm_num,
+revert h, norm_num,
+end
+
+
 theorem minus_one_quad_res_of_p {p : ℤ} (hp : prime_int p ∧ p ≠ 2) : (p ≡ 1 [ZMOD 4] ↔ (legendre_sym (-1: ℤ) hp) = 1) ∧ (p ≡ 3 [ZMOD 4] ↔ legendre_sym (-1 : ℤ) hp = (-1 : ℤ)) := sorry
 
 
@@ -41,26 +53,34 @@ rw int.coe_nat_mul, rw hab, rw hcd,
 ring,
 end 
 
-theorem one_mod_four_prime (p : ℤ)(h : prime_int p) : p ^ 2 ≡  1 [ZMOD 4] := sorry 
+theorem one_mod_four_prime (p : ℤ)(h : prime_int p ∧ p ≠ 2) : p ^ 2 ≡  1 [ZMOD 4] := sorry 
 
-theorem fermat_descent (p : ℤ)(h : prime_int p) : ∃ a b r : ℤ, (a ^ 2 + b ^ 2 = p * r) ∧ (1 < r) ∧ (r < p) → ∃ x y r' : ℤ, (1 ≤ r') ∧ (r'< r) ∧ (x ^ 2 + y ^ 2 = p * r') := 
+theorem fermat_descent (p : ℤ)(h : prime_int p ∧ p ≠ 2) : ∃ a b r : ℤ, (a ^ 2 + b ^ 2 = p * r) ∧ (1 < r) ∧ (r < p) → ∃ x y r' : ℤ, (1 ≤ r') ∧ (r'< r) ∧ (x ^ 2 + y ^ 2 = p * r') := 
 begin
 sorry 
 end 
 
-theorem fermat_two_square (p : ℤ)(h : prime_int p) : p ≡ 1 [ZMOD 4] → is_sum_of_two_squares (int.nat_abs p) := 
+theorem fermat_two_square (p : ℤ)(h : prime_int p ∧ p ≠ 2)(H: p ≥ 0) : p ≡ 1 [ZMOD 4] → is_sum_of_two_squares (int.nat_abs p) := 
 begin
 intro hpp, 
-have hp : prime_int p ∧ p ≠ 2, 
-begin 
-split, 
-exact h, 
-by_contradiction,
-sorry 
-end, 
-sorry 
-end   
+have b1 := (minus_one_quad_res_of_p h).1.1,
+have b2 := b1 hpp,
+have b3 := legendre_sym_one_implies_quadratic_res (-1) h b2,
+unfold quadratic_res at b3,
+rcases b3 with x,
+have b4 : 1 ≡ 1 [ZMOD ↑(int.nat_abs p)], by refl,
+have b5 := int.modeq.modeq_add b3_h b4, 
+have b6 : (-1 :ℤ) + 1 = 0, by simp,
+rw b6 at b5, 
+have b7 := int.modeq.symm b5,
+--rw nat_abs_of_nonneg at b7,
 
+
+--rw int.nat_abs_of_nonneg at b7,
+--have b8 := int.modeq.modeq_zero_iff.1 b7
+--exists_eq_mul_right_of_dvd
+sorry 
+end 
  
 
 --inductive less_than_or_equal (a : ℤ) : ℤ → Prop
