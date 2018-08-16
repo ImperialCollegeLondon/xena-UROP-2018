bafee988ed2173dfe33aa500d6ab86e3b4752c0b
guy <guy.leroy99@gmail.com>
2018-08-16 13:47:28 +0100

    refined order lemmas

--- src/M3P14/order.lean
+++ src/M3P14/order.lean
@@ -2,8 +2,8 @@ import data.nat.prime data.nat.gcd data.nat.modeq data.nat.gcd algebra.group chr
 
 open zmod nat
 
-/- Gives actual number when evaluating (e.g. #5 becomes 5) -/
-instance (n : ℕ) : has_repr (zmod n) := ⟨repr ∘ fin.val⟩ 
+@[simp] lemma units.coe_pow {α : Type*} [monoid α] (u : units α) (n : ℕ) : (↑(u ^ n) : α) = u ^ n :=
+by induction n; simp [*, _root_.pow_succ]
 
 -- Thanks Chris
 lemma order_of_dvd_of_pow_eq_one {d n : ℕ} [pos_nat n]  (a : units (zmod n)) (h : a ^ d = 1) : order_of a ∣ d :=
@@ -12,8 +12,7 @@ by_contradiction
 from nat.mod_lt _ (nat.pos_of_ne_zero (order_of_ne_zero _)))
 ⟨mt nat.dvd_of_mod_eq_zero h₁, by rwa ← pow_eq_mod_order_of⟩)
 
---set_option pp.all true
-/- define a function that given a n nats and coprime, gives back (a : units (zmod n)) -/  
+/- DEFINITIONS -/ 
 def units_zmod_mk (a n : ℕ ) (h : nat.coprime a n) [pos_nat n] : units (zmod n) := 
 {
     val := a,
@@ -24,19 +23,17 @@ def units_zmod_mk (a n : ℕ ) (h : nat.coprime a n) [pos_nat n] : units (zmod n
 
 def order_of_zmod (a n : ℕ) [pos_nat n] : ℕ := if h : nat.coprime a n then @order_of (units (zmod n)) _ _ _ (units_zmod_mk a n h) else 0
 
+def primitive_root (a n : ℕ) [pos_nat n] := coprime a n ∧ order_of_zmod a n = phi n
+
+/- THEOREMS -/ 
 theorem not_coprime_pow_mod (a d n : ℕ) (h1 : a ^ d ≡ 1 [MOD n]) (h2 : ¬coprime a n) : d = 0 := sorry
 
 -- is that true?
 theorem zmod_card_pow (a n d : ℕ) (h : coprime a n) [pos_nat n] : a^fintype.card (units (zmod n)) ≡ 1 [MOD n] := sorry
 
-@[simp] lemma units.coe_pow {α : Type*} [monoid α] (u : units α) (n : ℕ) : (↑(u ^ n) : α) = u ^ n :=
-by induction n; simp [*, _root_.pow_succ]
-
--- can we prove it without h?
---set_option pp.implicit true
---set_option pp.notation false
-theorem order_zmod_div (a n d : ℕ) (h : coprime a n) [pos_nat n] : a^d ≡ 1 [MOD n] → order_of_zmod a n ∣ d := 
+theorem order_zmod_div (a n d : ℕ) [pos_nat n] : a^d ≡ 1 [MOD n] → order_of_zmod a n ∣ d := 
 begin
+    cases (classical.em (nat.coprime a n)),
     intro h2,
     unfold order_of_zmod,
     cases (classical.em (nat.coprime a n)),
@@ -51,44 +48,25 @@ begin
           have h2' : (↑(a ^ d) : zmod n) = 1,
             rw h2,
             simp,
-          rw ←h2',simp,refl,
+            rw ←h2',simp,refl,
         },
         exact order_of_dvd_of_pow_eq_one (units_zmod_mk a n h) pow,
     },
     exact absurd h h_1,
+    intro pow,
+    rw [order_of_zmod, dif_neg h],
+    suffices : d = 0, simp [this],
+    exact not_coprime_pow_mod a d n pow h,
 end
-
-#print units
---lemma to prove phi and fincard are the same
-
-
-#eval fintype.card (units (zmod 15))
-
-#eval fintype.card (units (zmod 12))
-#eval order_of_zmod 7 12
-#eval 7^2 % 12
-
-#eval order_of_zmod 4 9
-#eval 4^3 % 9
-
-#check prime.dvd_iff_not_coprime
-#check modeq.dvd_of_modeq
-#check order_of_dvd_card_univ
-#check fintype.card
-#check fintype.elems (units (zmod 10))
-#check dvd_trans
-#check (units_zmod_mk 5 7) 
 
 theorem order_zmod_div_phi_n (a n : ℕ) (h : coprime a n) [pos_nat n] : order_of_zmod a n ∣ phi n :=
 begin
     have : a ^ (phi n) ≡ 1 [MOD n], from euler_phi_thm a n h,
-    exact order_zmod_div a n (phi n) h this,
+    exact order_zmod_div a n (phi n) this,
 end
 
 theorem pow_order_units_zmod_eq_one (a n : ℕ) [pos_nat n] (h : coprime a n) : (units_zmod_mk a n h) ^ order_of (units_zmod_mk a n h) = 1 :=
 pow_order_of_eq_one (units_zmod_mk a n h)
-
-
 
 theorem pow_order_zmod_eq_one (a n : ℕ) [pos_nat n] : (a : zmod n) ^ order_of_zmod a n = (1 : zmod n) :=
 begin
@@ -102,10 +80,6 @@ begin
     rw dif_neg em,
     refl,
 end
-
-
-
-def primitive_root (a n : ℕ) [pos_nat n] := coprime a n ∧ order_of_zmod a n = phi n
 
 theorem primitive_root_existence (n : ℕ) [pos_nat n] : ∃ a : ℕ, (primitive_root a n) ↔ n = 1 ∨ n = 2 ∨ n = 4 ∨ ∃ p r : ℕ, prime p ∧ r > 0 → (n = p^r ∨ n = 2*p^r) := sorry
 
