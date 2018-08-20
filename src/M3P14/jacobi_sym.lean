239dc7f095ba0da25fb45f081f17be51a55384f5
guy <guy.leroy99@gmail.com>
2018-08-20 15:14:05 +0100

    added def of jacobi symbol

--- src/M3P14/jacobi_sym.lean
+++ src/M3P14/jacobi_sym.lean
@@ -1,7 +1,7 @@
 import data.nat.basic M3P14.order_zmodn_kmb data.int.basic
 
-theorem aux1 (a : ℕ) (b : ℤ) : 
-    1 < nat.succ (nat.succ a):= 
+private theorem aux1 (a : ℕ): 
+    1 < nat.succ (nat.succ a) := 
 begin
     suffices : 1 < (nat.succ a) + 1, by simp [this],
     suffices : 0 < (nat.succ a), rwa lt_add_iff_pos_left,
@@ -9,65 +9,66 @@ begin
     trivial,
 end
 
-#check nat.pos_iff_ne_zero
+private theorem aux2 (a b : ℕ) (h : a + 3 ≥ int.nat_abs ↑b) : 
+    (a + 3) % b < nat.succ (nat.succ (nat.succ a)) := 
+begin
+    sorry,
+end
 
-def jacobi_sym_pos : ℕ → ℕ → ℤ
+private theorem aux3 (a b : ℕ) (h : ¬a + 3 ≥ int.nat_abs ↑b) : 
+    2 < nat.succ (nat.succ (nat.succ a)) := 
+begin
+    sorry,
+end
+
+private theorem aux4 (a b : ℕ) : 
+    (a + 3) / 2 < nat.succ (nat.succ (nat.succ a)) := 
+begin
+    sorry,
+end
+
+private theorem aux5 (a b : ℕ) (h : ¬a + 3 ≥ int.nat_abs ↑b) : 
+    b < nat.succ (nat.succ (nat.succ a)) := 
+begin
+    sorry,
+end
+
+private def jacobi_sym_pos : ℕ → ℕ → ℤ
 | 0          b := 0
-| a     -[1+b] := 0
 | 1          b := 1
-| (-1)       b := if b % 4 = 1 then 1 else -1 
 | 2          b := if b % 8 = 1 ∨ b % 8 = 7 then 1 else -1
-| (nat.succ (nat.succ a)) b := 
-                --have int.nat_abs 2 < int.nat_abs (1 + (1 + ↑a)), by sorry,
-                --have int.nat_abs (↑a / 2 % b) < int.nat_abs (1 + (1 + ↑a)), by sorry,
-                --have int.nat_abs (b % ↑a) < int.nat_abs (1 + (1 + ↑a)), by sorry,
-                if h : (nat.succ (nat.succ a)) % 2 = 0 then jacobi_sym_pos 2 b * jacobi_sym_pos (((nat.succ (nat.succ a))/2) % b) b else 
-                (if (nat.succ (nat.succ a)) % 4 = 1 ∨ b % 4 = 1 then jacobi_sym_pos (b % (nat.succ (nat.succ a))) (nat.succ (nat.succ a)) else -(jacobi_sym_pos (b % (nat.succ (nat.succ a))) (nat.succ (nat.succ a))))
+| (nat.succ (nat.succ (nat.succ a))) b := 
+                if h1 : (a+3) ≥ int.nat_abs b then 
+                have (a + 3) % b < nat.succ (nat.succ (nat.succ a)), from aux2 a b h1, 
+                jacobi_sym_pos ((a+3)%b) b else
+                (if h2 : (a+3) % 2 = 0 then 
+                have 2 < nat.succ (nat.succ (nat.succ a)), from aux3 a b h1, 
+                have (a + 3) / 2 < nat.succ (nat.succ (nat.succ a)), from aux4 a b, 
+                jacobi_sym_pos 2 b * jacobi_sym_pos ((a+3)/2) b else 
+                have b < nat.succ (nat.succ (nat.succ a)), from aux5 a b h1,
+                (if (a+3) % 4 = 1 ∨ b % 4 = 1  then jacobi_sym_pos b (a+3) else -(jacobi_sym_pos b (a+3))))
+using_well_founded {rel_tac:= λ _ _, `[exact ⟨_, measure_wf (psigma.fst)⟩ ]}
 
+private def jacobi_sym_aux : ℤ → ℤ → ℤ
+| a     -[1+b] := 0
+| (-1)       b := if b % 4 = 1 then 1 else -1 
+| -[1+(nat.succ a)] b := 
+                have 1 < nat.succ (nat.succ a), from aux1 a, 
+                jacobi_sym_pos (a+2) (int.nat_abs b) * jacobi_sym_aux (-1) b
+| a          b := jacobi_sym_pos (int.nat_abs a) (int.nat_abs b)
+using_well_founded {rel_tac:= λ _ _, `[exact ⟨_, measure_wf (int.nat_abs ∘ psigma.fst)⟩ ]}
 
+/- Computes the Jacobi Symbol, extended to b even which will output 0, is it the Kronecker Symbol?-/
 def jacobi_sym : ℤ → ℤ → ℤ
-| 0          b := 0
-| a     -[1+b] := 0
-| 1          b := 1
-| (-1)       b := if b % 4 = 1 then 1 else -1 
-| 2          b := if b % 8 = 1 ∨ b % 8 = 7 then 1 else -1
-| -[1+(nat.succ (nat.succ a))]     b := jacobi_sym_pos (nat.succ (nat.succ a)) b * jacobi_sym (-1) b
-| (nat.succ (nat.succ a))          b := jacobi_sym_pos (nat.succ (nat.succ a)) b
+| a          b := if b % 2 = 1 then jacobi_sym_aux a b else 0
 
-
-using_well_founded{rel_tac:= λ _ _, `[exact ⟨_, measure_wf (int.nat_abs∘ psigma.fst)⟩ ]}
-
-
-
-#exit
-
--- make it handle neg number once, then do all the positive stuff
-def jacobi_sym : ℤ → ℤ → ℤ
-| a     -[1+b] := 0
-| 0          b := 0
-| 1          b := 1
-| (-1)       b := if b % 4 = 1 then 1 else -1 
-| 2          b := if b % 8 = 1 ∨ b % 8 = 7 then 1 else -1
-| -[1+ (nat.succ a)]     b := 
-                have int.nat_abs (int.succ ↑a % b) < nat.succ (nat.succ a), begin sorry end,
-                have 1 < nat.succ (nat.succ a), from aux1 a b,
-                jacobi_sym (int.succ a % b) b * jacobi_sym (-1) b
-| (nat.succ (nat.succ a)) b := 
-                have int.nat_abs 2 < int.nat_abs (1 + (1 + ↑a)), by sorry,
-                have int.nat_abs (↑a / 2 % b) < int.nat_abs (1 + (1 + ↑a)), by sorry,
-                have int.nat_abs (b % ↑a) < int.nat_abs (1 + (1 + ↑a)), by sorry,
-                if h : a % 2 = 0 then jacobi_sym 2 b * jacobi_sym ((a/2) % b) b else 
-                (if a % 4 = 1 ∨ b % 4 = 1 then jacobi_sym (b % a) a else -(jacobi_sym (b % a) a))
-
-using_well_founded{rel_tac:= λ _ _, `[exact ⟨_, measure_wf (int.nat_abs∘ psigma.fst)⟩ ]}
 
 #eval jacobi_sym 1 (-5 : ℤ)
-
 #eval jacobi_sym (-5 : ℤ) (-4 : ℤ)
-
 #eval jacobi_sym (-1 : ℤ) 9
 #eval jacobi_sym (-2 : ℤ) 15
-
 #eval jacobi_sym (-5 : ℤ) 8
+#eval jacobi_sym 1236 200011
+
 
 
