3e9facafa6874dbe03fd2137d1bab428181d15bc
guy <guy.leroy99@gmail.com>
2018-08-21 14:39:09 +0100

    cleaned up jacobi defs

--- src/M3P14/jacobi_sym.lean
+++ src/M3P14/jacobi_sym.lean
@@ -1,12 +1,6 @@
 import data.nat.basic M3P14.order_zmodn_kmb data.int.basic M3P14.lqr
 
-private theorem aux1 (a : ℕ): 
-    1 < nat.succ (nat.succ a) := dec_trivial
-
-private theorem aux2_bis (a : ℕ) : 
-    0 < nat.succ a := dec_trivial
-
-private theorem aux2_ter (a b : ℕ) (h : ¬a + 3 = b + 1) : 
+private theorem aux1 (a b : ℕ) (h : ¬a + 3 = b + 1) : 
     b + 1 ≠ a + 3 := 
 begin
     suffices : b + 1 = a + 3 → false, by simp [this],
@@ -21,25 +15,16 @@ begin
     suffices : (a + 3) % (b + 1) < (a + 3), by simp [this],
     cases (classical.em (a + 3 = b + 1)),
     rw [h_1.symm, nat.mod_self],
-    exact aux2_bis (nat.succ (nat.succ a)),
+    exact dec_trivial,
     have eq : int.nat_abs (1 + ↑b) = 1 + b, from int.nat_abs_of_nat (1 + b),
     have h2 : a + 3 ≥ b + 1, rw eq at h, rwa add_comm b 1,
     suffices : (nat.succ b) < nat.succ (nat.succ (nat.succ a)), from lt_trans (nat.mod_lt (a + 3) (nat.pos_iff_ne_zero.mpr (by trivial))) this,
     suffices : b + 1 < a + 3, by simp [this],
-    suffices : b + 1 ≤ a + 3, from lt_of_le_of_ne this (aux2_ter a b h_1),
+    suffices : b + 1 ≤ a + 3, from lt_of_le_of_ne this (aux1 a b h_1),
     exact h2,
 end
 
-private theorem aux3 (a : ℕ) : 
-    2 < nat.succ (nat.succ (nat.succ a)) := dec_trivial
-
-private theorem aux4 (a b : ℕ) : 
-    (a + 3) / 2 < nat.succ (nat.succ (nat.succ a)) := 
-begin
-    exact nat.div_lt_self dec_trivial dec_trivial,
-end
-
-private theorem aux5 (a b : ℕ) (h : ¬a + 3 ≥ int.nat_abs ↑b) : 
+private theorem aux3 (a b : ℕ) (h : ¬a + 3 ≥ int.nat_abs ↑b) : 
     b % (a + 3) < nat.succ (nat.succ (nat.succ a)) := 
 begin
     simp at h,
@@ -55,13 +40,13 @@ private def jacobi_sym_pos : ℕ → ℕ → ℤ
 | 2          (nat.succ b) := if (nat.succ b) % 8 = 1 ∨ (nat.succ b) % 8 = 7 then 1 else -1
 | (nat.succ (nat.succ (nat.succ a))) (nat.succ b) := 
                 if h1 : (a+3) ≥ int.nat_abs (nat.succ b) then 
-                have (a + 3) % (nat.succ b) < nat.succ (nat.succ (nat.succ a)), begin sorry end, 
+                have (a + 3) % (nat.succ b) < nat.succ (nat.succ (nat.succ a)), from aux2 a b h1, 
                 jacobi_sym_pos ((a+3)%(nat.succ b)) (nat.succ b) else
                 (if h2 : (a+3) % 2 = 0 then 
-                have 2 < nat.succ (nat.succ (nat.succ a)), from aux3 a, 
-                have (a + 3) / 2 < nat.succ (nat.succ (nat.succ a)), from aux4 a (nat.succ b), 
+                have 2 < nat.succ (nat.succ (nat.succ a)), from dec_trivial, 
+                have (a + 3) / 2 < nat.succ (nat.succ (nat.succ a)), from nat.div_lt_self dec_trivial dec_trivial, 
                 jacobi_sym_pos 2 (nat.succ b) * jacobi_sym_pos ((a+3)/2) (nat.succ b) else 
-                have (nat.succ b) % (a + 3) < nat.succ (nat.succ (nat.succ a)), begin sorry end,
+                have (nat.succ b) % (a + 3) < nat.succ (nat.succ (nat.succ a)), from aux3 a (nat.succ b) h1,
                 (if (a+3) % 4 = 1 ∨ (nat.succ b) % 4 = 1  then jacobi_sym_pos ((nat.succ b) % (a+3)) (a+3) else -(jacobi_sym_pos ((nat.succ b) % (a+3)) (a+3))))
 using_well_founded {rel_tac:= λ _ _, `[exact ⟨_, measure_wf (psigma.fst)⟩ ]}
 
@@ -69,7 +54,7 @@ private def jacobi_sym_aux : ℤ → ℤ → ℤ
 | a     -[1+b] := 0
 | (-1)       b := if b % 4 = 1 then 1 else -1 
 | -[1+(nat.succ a)] b := 
-                have 1 < nat.succ (nat.succ a), from aux1 a, 
+                have 1 < nat.succ (nat.succ a), from dec_trivial, 
                 jacobi_sym_pos (a+2) (int.nat_abs b) * jacobi_sym_aux (-1) b
 | a          b := jacobi_sym_pos (int.nat_abs a) (int.nat_abs b)
 using_well_founded {rel_tac:= λ _ _, `[exact ⟨_, measure_wf (int.nat_abs ∘ psigma.fst)⟩ ]}
@@ -107,4 +92,4 @@ theorem jacobi_num_neg_one (n : ℤ) (n_pos_odd : n > 0 ∧ int.gcd 2 n = 1) [ha
 
 theorem jacobi_num_two (n : ℤ) (n_pos_odd : n > 0 ∧ int.gcd 2 n = 1) [has_pow ℤ ℤ] : jacobi_sym 2 n = (-1)^(((n^2)-1)/8) := sorry
 
-theorem jacobi_denom_one (a : ℤ) : jacobi_sym a 1 = 1 := sorry
+theorem jacobi_denom_one (a : ℤ) : jacobi_sym a 1 = 1 := by refl
